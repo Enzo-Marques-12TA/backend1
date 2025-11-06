5fc6159315cdf33070a29cdceb1bb2b62dc6fddf
Enzo Marques 12TA <enzoflashiron@gmail.com>
2025-11-05 22:55:39 -0300

    Similar ao frontend, também término da segunda produção áudio-visual

--- src/index.js
+++ src/index.js
@@ -6,10 +6,21 @@ const app = express()
 const port = 3333
 
 app.use(cors())
+app.use(express.json())
 
 // GET, POST, PUT, DELETE, PATCH, PUT
 app.get("/", (request, response) => {
     response.json(persons)
+})
+
+app.post("/cadastrar", (request, response) => {
+    const { name, email, age, nickname, password } = request.body.user
+
+    console.log(`${name}, ${email}, ${age}, ${nickname}, ${password}`)
+
+    //cadastar o usuário no backend
+
+    response.status(201).json({ message: "Usuário cadastrado com sucesso! "})
 })
 
 app.listen(port, () => {
