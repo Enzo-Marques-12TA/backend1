b7e262f991e554be660a19d6cdc8310c8b00901c
Enzo Marques 12TA <enzoflashiron@gmail.com>
2025-11-18 23:35:04 -0300

    Video 1 finalizado, porém algum erro surgiu, necessário consultar o indefectível Marcio, e lembrar de refazer o env na aula

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import express from "express"
 import cors from "cors"
+import mysql from "mysql2"
 import { persons } from "./persons.js"
+
+const { DB_NAME, DB_USER, DB_PASSWORD, DB_HOST} = process.env
 
 const app = express()
 const port = 3333
@@ -16,13 +19,30 @@ app.get("/", (request, response) => {
 app.post("/cadastrar", (request, response) => {
     const { name, email, age, nickname, password } = request.body.user
 
-    console.log(`${name}, ${email}, ${age}, ${nickname}, ${password}`)
-
     //cadastar o usuário no backend
+    const insertCommand = `
+    INSERT INTO enzomarques_02ta(name, email, age, nickname, password)
+    VALUES (?, ?, ?, ?, ?)
+    `
 
-    response.status(201).json({ message: "Usuário cadastrado com sucesso! "})
+    database.query(insertCommand, [name, email, age, nickname, password], (error) => {
+        if(error) {
+            console.log(error)
+            return
+        }
+
+        response.status(201).json({ message: "Usuário cadastrado com sucesso! "})
+    })
 })
 
 app.listen(port, () => {
     console.log(`Servidor rodando na porta ${port}!`)
+})
+
+const database = mysql.createPool({
+    host: DB_HOST,
+    user: DB_USER,
+    password: DB_PASSWORD,
+    database: DB_NAME,
+    connectionLimit: 10
 })
